f5cf0434577d7668ccc427d80ce666e67138429e
aashikmuhammad <aashikmuhammad4@gmail.com>
2020-05-19 17:59:13 +0530

    footer align

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg')
-//const connectionString = 'postgres://eqsewrxknjnjup:3bc0811484fe88b03797d5f845160ed663f503792c6c52298d12579da399fb2f@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d5sr5fpoj7mub'
-const connectionString = 'postgressql://postgres:alhamdulillah@localhost:5432/blood donors'
+const connectionString = 'postgres://eqsewrxknjnjup:3bc0811484fe88b03797d5f845160ed663f503792c6c52298d12579da399fb2f@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d5sr5fpoj7mub'
+// const connectionString = 'postgressql://postgres:alhamdulillah@localhost:5432/blood donors'
 // const connectionString = 'postgressql://postgres1:Aashiq@1310@localhost:5432/blood donars'
 const client = new Client({ connectionString: connectionString}) 
 async function connect(){
